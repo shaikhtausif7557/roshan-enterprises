c2b0bbfbdca3fa253d37fa18cd6c8286a267532f
shaikhtausif7557 <shaikhtausif7557@gmail.com>
2024-12-09 18:09:24 +0530

    all

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import projectRoutes from './models/project.js';
+import projectRoutes from './models/project.cjs';
 
 dotenv.config();
 
